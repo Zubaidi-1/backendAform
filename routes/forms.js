ff6172ad2a45d680d27c20bd05e511707a40a205
Zubaidi-1 <abdallazubaidi7@gmail.com>
2024-12-18 16:08:28 +0300

    auth

--- routes/forms.js
+++ routes/forms.js
@@ -3,40 +3,41 @@ const loginController = require("../controllers/Login");
 const formsController = require("../controllers/forms");
 const auth = require("../middlewares/auth"); // Import the auth middleware
 const router = express.Router();
+const adminAuth = require("../middlewares/authAdmin");
 
 // Protected routes using auth middleware
 router.post("/aform/Refunds", auth, formsController.submitRefund);
 
 router.post("/aform/P&P", auth, formsController.submitPP);
-router.get("/aform/P&P", auth, formsController.getPP);
-router.patch("/pp", auth, formsController.updateFinished);
+router.get("/aform/P&P", adminAuth, formsController.getPP);
+router.patch("/pp", adminAuth, formsController.updateFinished);
 router.post("/aform/WE", auth, formsController.submitWe);
-router.get("/aform/WE", auth, formsController.getWE);
-router.patch("/WE", auth, formsController.updateFinishedWE);
+router.get("/aform/WE", adminAuth, formsController.getWE);
+router.patch("/WE", adminAuth, formsController.updateFinishedWE);
 router.post("/aform/Lost", auth, formsController.submitLost);
-router.get("/lost", auth, formsController.getLOST);
-router.patch("/lost", auth, formsController.updateFinishedLost);
+router.get("/lost", adminAuth, formsController.getLOST);
+router.patch("/lost", adminAuth, formsController.updateFinishedLost);
 
 router.post("/aform/LE", auth, formsController.submitLE);
-router.get("/LE", auth, formsController.getLE);
-router.patch("/LE", auth, formsController.updateFinishedListing);
+router.get("/LE", adminAuth, formsController.getLE);
+router.patch("/LE", adminAuth, formsController.updateFinishedListing);
 router.post("/aform/REF", auth, formsController.submitREF);
-router.get("/REF", auth, formsController.getRef);
-router.patch("/REF", auth, formsController.updateFinishedRef);
+router.get("/REF", adminAuth, formsController.getRef);
+router.patch("/REF", adminAuth, formsController.updateFinishedRef);
 router.post("/aform/DEF", auth, formsController.submitDEF);
-router.get("/DEF", auth, formsController.getDEF);
-router.patch("/DEF", auth, formsController.updateFinishedDefective);
+router.get("/DEF", adminAuth, formsController.getDEF);
+router.patch("/DEF", adminAuth, formsController.updateFinishedDefective);
 router.post("/aform/PWDC", auth, formsController.submitPWDC);
-router.get("/PWDC", auth, formsController.getPWDC);
-router.patch("/PWDC", auth, formsController.updateFinishedPwdc);
+router.get("/PWDC", adminAuth, formsController.getPWDC);
+router.patch("/PWDC", adminAuth, formsController.updateFinishedPwdc);
 router.post("/aform/SS", auth, formsController.submitSS);
-router.get("/SS", auth, formsController.getSaved);
-router.patch("/SS", auth, formsController.updateFinishedSaved);
+router.get("/SS", adminAuth, formsController.getSaved);
+router.patch("/SS", adminAuth, formsController.updateFinishedSaved);
 router.post("/aform/SPA", auth, formsController.submitSPA);
-router.get("/SPA", auth, formsController.getSpanish);
-router.patch("/SPA", auth, formsController.updateFinishedSpanish);
+router.get("/SPA", adminAuth, formsController.getSpanish);
+router.patch("/SPA", adminAuth, formsController.updateFinishedSpanish);
 router.post("/aform/CC", auth, formsController.submitCC);
-router.get("/CC", auth, formsController.getContact);
-router.patch("/CC", auth, formsController.updateFinishedContact);
+router.get("/CC", adminAuth, formsController.getContact);
+router.patch("/CC", adminAuth, formsController.updateFinishedContact);
 
 module.exports = router;
